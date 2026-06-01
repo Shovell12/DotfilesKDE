a50911b20d73ea6112f10a0ec735caa877cac410
Shovell12 <gusatvomosquera831@hotmail.com>
2026-05-31 23:00:17 -0500

    backup: 2026-05-31 23:00

--- g733/g733-monitor.sh
+++ g733/g733-monitor.sh
@@ -4,15 +4,17 @@ HEADSET_WAS_OFF=true
 
 while true; do
     BATTERY=$(headsetcontrol -b 2>/dev/null | grep "Level:" | grep -o '\-\?[0-9]*%' | tr -d '%')
-    
+
     if [ -n "$BATTERY" ] && [ "$BATTERY" -gt 0 ] 2>/dev/null; then
         if $HEADSET_WAS_OFF; then
-            sleep 2
+            sleep 1
+            headsetcontrol -l 0
+            sleep 1
             headsetcontrol -l 0
             HEADSET_WAS_OFF=false
         fi
     else
         HEADSET_WAS_OFF=true
     fi
-    sleep 5
+    sleep 2
 done
